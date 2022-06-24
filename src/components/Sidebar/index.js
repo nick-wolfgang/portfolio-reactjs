7cfb25f82755976e2e8418a10c8e49821c129ad7
nick-Wolfgang <nicklarsonwolfgang@gmail.com>
2022-06-24 21:36:36 +0200

    sidebar & layout components added

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.scss'
+import { Link } from 'react-router-dow'
+
+function Sidebar() {
+  return (
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+            
+            </Link>
+        </div>
+    )
+}
+
+export default Sidebar;
