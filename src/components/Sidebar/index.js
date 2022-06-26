12d341d8a8f7a08aaa4d8ac694a486c1ca2f889d
nick-Wolfgang <nicklarsonwolfgang@gmail.com>
2022-06-26 22:34:26 +0200

    Sidebar finished1

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,8 @@ import './index.scss'
 import LogoS from './../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope, faLink } from '@fortawesome/free-solid-svg-icons'
+import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Sidebar() {
   return (
@@ -14,7 +15,7 @@ function Sidebar() {
             <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink exact="true" activeclassname="active" className="home-link" to="/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
@@ -24,6 +25,35 @@ function Sidebar() {
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+          <li>
+            <a 
+              className='profile-links'
+              target="_blank" 
+              rel="noreferrer" 
+              href='https://www.linkedin.com'>
+              <FontAwesomeIcon icon={faLink} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a 
+              className='profile-links'
+              target="_blank" 
+              rel="noreferrer" 
+              href='https://www.github.com/nick-wolfang'>
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a 
+              className='profile-links'
+              target="_blank" 
+              rel="noreferrer" 
+              href='https://www.linkedin.com'>
+              <FontAwesomeIcon icon={faDiscord} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
     </div>
   )
 }
